5f17808e604ab9487a15335afdf262c53675b961
masudbappy <masud.bappy90@gmail.com>
2023-01-15 22:05:12 +0600

    calling through api gateway.

--- src/main/java/com/masudbappy/feignclients/AddressFeignClient.java
+++ src/main/java/com/masudbappy/feignclients/AddressFeignClient.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(url = "${address.service.url}", value = "address-feign-client")
-@FeignClient( value = "address-service", path = "/api/address")
+@FeignClient( value = "api-gateway")
 public interface AddressFeignClient {
 
-    @GetMapping("/getById/{id}")
+    @GetMapping("/address-service/api/address/getById/{id}")
     public AddressResponse getById(@PathVariable("id") long id);
 
 }
